382f0f6eba41cabc3ef29fcf405b19d542065fa3
Cezar Coca <cezarcoca@gmail.com>
2014-07-01 10:22:31 +0300

    Adjust the chart size to better fit the screen.

--- src/main/java/org/sonar/plugins/toxicity/chart/ToxicityChart.java
+++ src/main/java/org/sonar/plugins/toxicity/chart/ToxicityChart.java
@@ -60,8 +60,8 @@ public class ToxicityChart extends AbstractChart {
       .getLogger(ToxicityChart.class);
 
   static final String KEY = "toxicitychart";
-  private static final int DEFAULT_WIDTH = 850;
-  private static final int DEFAULT_HEIGHT = 576;
+  private static final int DEFAULT_WIDTH = 550;
+  private static final int DEFAULT_HEIGHT = 410;
   private static final Float X_AXIS_FONT_SIZE = Float.valueOf("7");
 
   @VisibleForTesting
